7f6ba8f5738744906d8d30b393b60c34c7d4f5fc
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-03-09 09:11:25 -0600

    working with firebase

--- app/app.js
+++ app/app.js
@@ -2,25 +2,57 @@
 console.log("App.js is connected!");
 
 /*
-An Angular Application that allows users to create, save, edit, and delete notes
-Each user wil have their own separate database within Firebase
-There are 4 different screen displays
-	1. Login
-	2. Register
-	3. List All Notes
-	4. Create New Notes
+An Angular application designed to allow users the ability to create a 
+field journal of drink experiences. 
 
-Each page has a controller directly related to it.
+The app will attach to firebase to store all of the user's information.
+The user's information will include:
+	
+		Field Journal,
+		Recipes Information,
+		Personal Information,
+		Ratings and Reviews for specific Locations
 */
 
 var app = angular.module("MyPretentiousCup", [
-	'ui.router',
-	'ngMaterial'
-]);
+						'ui.router',
+						'ngMaterial'
+])
 
-app.config(function($stateProvider, $urlRouterProvider) {
+.service('fbRef', function(FBCreds) {
+	return new Firebase(FBCreds.databaseURL);
+})
 
-				$urlRouterProvider.otherwise('/home');
+.service('fbAuth', function($q, $firebase, $firebaseAuth, fbRef) {
+				var auth;
+				return function() {
+					if (auth) return $q.when(auth);
+					var authObj = $firebaseAuth(fbRef);
+					if (authObj.$getAuth()) {
+						return $q.when(auth = authObj.$getAuth());
+					}
+
+					var deferred = $q.defer();
+					authObj.$authAnonymously().then(function(authData) {
+							auth = authData;
+							console.log(authData);
+							deferred.resolve(authData);
+					});
+					return deferred.promise;
+				};
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+				console.log("I am within the config");
+
+				var checkIfAuth = {
+					projects: function(fbAuth) {
+						return fbAuth();
+					}
+				};
+
+				$urlRouterProvider.otherwise('/landing');
 
 				$stateProvider
         
@@ -43,33 +75,39 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		        .state('home', {
 		            url: '/home',
 		            templateUrl: '../partials/Home.html',
-		            controller: "HomeCtrl"
+		            controller: "HomeCtrl",
+		            resolve: {checkIfAuth}
 		        })
 		        .state('field-journal', {
 		            url: '/field-journal',
 		            templateUrl: '../partials/FieldJournal.html',
-		            controller: "FieldJournalCtrl"
+		            controller: "FieldJournalCtrl",
+		            resolve: {checkIfAuth}
 		        })
 		        .state('recipes', {
 		            url: '/recipes',
 		            templateUrl: '../partials/Recipes.html',
-		            controller: "RecipesCtrl"
+		            controller: "RecipesCtrl",
+		            resolve: {checkIfAuth}
 		        })
 		        .state('drinking-buddies', {
 		            url: '/drinking-buddies',
 		            templateUrl: '../partials/DrinkingBuddies.html',
-		            controller: "DrinkingBuddiesCtrl"
+		            controller: "DrinkingBuddiesCtrl",
+		            resolve: {checkIfAuth}
 		        })
 		        .state('globe-view', {
 		            url: '/globe-view',
 		            templateUrl: '../partials/GlobeView.html',
-		            controller: "GlobeViewCtrl"
-		        })
-	
-});
+		            controller: "GlobeViewCtrl",
+		            resolve: {checkIfAuth}
+		        });
 
-app.run(() => {
-	console.log("You are connected");
+	
+})
+
+.run(() => {
+						console.log("You are connected");
 });
 
 
